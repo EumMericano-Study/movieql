facd155afd37161f69130bafda5b0388efd72580
Eum <opwer032@naver.com>
2022-01-03 22:36:47 +0900

    refactor: Remove all content for new leanring

--- graphql/db.js
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-
-const API_URL = "https://yts.mx/api/v2/list_movies.json";
-
-const getMovies = (limit, rating) => {
-  fetch(`${API_URL}`).then((res) => res.json());
-};
