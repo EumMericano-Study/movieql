ea37921a718a34fcbe8df788bd77c67e6bef43ab
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-06 07:19:06 +0900

    feat: getMovieDetail, getSUggestions 추가

--- graphql/db.js
+++ graphql/db.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const BASE_URL = "https://yts.mx/api/v2/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAIL_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
 export const getMovies = async (limit, rating) => {
     let REQUEST_URL = LIST_MOVIES_URL;
@@ -15,5 +17,27 @@ export const getMovies = async (limit, rating) => {
             minimum_rating: rating,
         },
     });
+    return movies;
+};
+
+export const getMovie = async (id) => {
+    let REQUEST_URL = MOVIE_DETAIL_URL;
+
+    const {
+        data: {
+            data: { movie },
+        },
+    } = await axios(REQUEST_URL, { params: { movie_id: id } });
+    return movie;
+};
+
+export const getSuggestions = async (id) => {
+    let REQUEST_URL = MOVIE_SUGGESTIONS_URL;
+
+    const {
+        data: {
+            data: { movies },
+        },
+    } = await axios(REQUEST_URL, { params: { movie_id: id } });
     return movies;
 };
